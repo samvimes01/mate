f1dce3809d4f7c4a1e65c4b502142edb45cb9a4b
SamVimes0 <k.aleksandr88@gmail.com>
2019-01-20 21:42:21 +0200

    autocomplete readme

--- lesson-autocomplete/README.md
+++ lesson-autocomplete/README.md
@@ -1,6 +1,6 @@
 # Autocomplete
 
 1. [Working version](https://samvimes01.github.io/mate/lesson-autocomplete/index.html)
-2. [Pull request JS]()
-3. [Pull request HTML]()
-4. [Pull request  CSS]()
+2. [Pull request JS](https://github.com/samvimes01/mate/compare/devA...gh-pages?expand=1#diff-e29325954341832e7b56c81888767bf7)
+3. [Pull request HTML](https://github.com/samvimes01/mate/compare/devA...gh-pages?expand=1#diff-559337a6190f907ab433c080274c38a8)
+4. [Pull request  CSS](https://github.com/samvimes01/mate/compare/devA...gh-pages?expand=1#diff-826ae9a0e16e7c4e8bd53b043e94eca7)
--- lesson-autocomplete/lib/script.js
+++ lesson-autocomplete/lib/script.js
@@ -1,4 +1,9 @@
 'use strict';
+/**
+ * Author: Oleksandr Korneiko
+ * Unit: Autocomplete
+ **/
+
 const parentSelector = '.material-select';
 const parent = document.querySelector(`${parentSelector}`);
 const input = parent.querySelector(`${parentSelector}__input`);
@@ -19,16 +24,17 @@ let selected = new Set();
 const wrapper = debounce(onChange, 1000);
 
 input.addEventListener('input', wrapper);
-input.addEventListener('keyup', addInput);
-input.addEventListener('focus', moveLabel);
+input.addEventListener('keyup', addInput); // hit Enter on input value that isn't in the suggestions list - adds this value
+input.addEventListener('focus', moveLabel); // just visual effects
 input.addEventListener('blur', moveLabel);
 document.addEventListener('click', moveLabel);
-selectedElems.addEventListener('click', delChip);
+selectedElems.addEventListener('click', delChip); // delete chip item (selected value)
 suggestionElems.addEventListener('click', selectItem);
 suggestionElems.addEventListener('mouseleave', hideSuggestions);
 suggestionElems.addEventListener('keyup', selectItemByKey);
 send.addEventListener('click', sendSelected);
 
+// this function debounced. after delay it shows list with suggeste items
 function onChange(event) {
   const value = event.target.value;
   if (!value) return;
@@ -45,16 +51,19 @@ function onChange(event) {
   suggestionElems.firstElementChild.focus();
 }
 
+// hit Enter to add input.value to Chips (selected items)
 function addInput(event) {
   if (event.keyCode === 13) {
     addItem(event.target.value);
   }
 }
 
+// delete Chip
 function delChip(event) {
   event.isHandled = true;
   if (!event.target.classList.contains('mat-chip__close-btn')) return;
   const chip = event.target.closest('.mat-chip');
+  // may be not the clearest realisation - last letter of Chips - always X - it's instead of close btn, so we slice this letter
   selected.delete(chip.textContent.slice(0, -1));
   chip.remove();
 }
@@ -68,12 +77,14 @@ function hideSuggestions(event) {
   moveLabel();
 }
 
+// select item from suggestions list after click
 function selectItem(event) {
   event.isHandled = true;
   if (!event.target.closest('.material-select__autocomplete-suggestion')) return;
   addItem(event.target.textContent);
 }
 
+// select item from suggestions list after arrow Up/Down, and Enter
 function selectItemByKey(event) {
   const parent = event.target.parentElement;
   if ((event.keyCode === 38 && event.target === parent.firstElementChild) ||
@@ -91,6 +102,7 @@ function selectItemByKey(event) {
   }
 }
 
+// useless function to pretend that we send selected Chips to server or elsewhere
 function sendSelected(event) {
   hideSuggestions(event);
   clearDom(selectedElems);
@@ -104,18 +116,21 @@ function sendSelected(event) {
   moveLabel();
 }
 
+// instead of data from array - this function may get data from server
 function getSuggestions(value) {
   const result = availableTags.filter(el => el.toLocaleLowerCase().includes(value.toLocaleLowerCase()));
   if (result.length === 0) return false;
   return result;
 }
 
+// utility function to clear suggestions list
 function clearDom(node) {
   while (node.firstChild) {
     node.removeChild(node.firstChild);
   }
 }
 
+// just for visual effects - moves label from input field
 function moveLabel() {
   if (input ===  document.activeElement || (selected.size !== 0 || input.value !== '')) {
     label.style.position = 'static';
@@ -127,6 +142,7 @@ function moveLabel() {
   }
 }
 
+// adds selected item to set and chips span
 function addItem(value) {
   if (selected.has(value)) return;
   selected.add(value);
@@ -134,6 +150,7 @@ function addItem(value) {
   selectedElems.insertAdjacentHTML('beforeEnd', suggestion);
 }
 
+// this function copypasted from previous task, which Misha marked as Done
 function debounce(f, delay) {
   let timerId;
   return function() {
